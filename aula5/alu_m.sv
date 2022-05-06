1744b5e48f99b25608940097313f56a143824ffe
danielmsousa <54556367+DanielMSousa@users.noreply.github.com>
2022-05-06 15:40:11 -0300

    Add files via upload

--- aula5/alu_m.sv
+++ aula5/alu_m.sv
@@ -1,50 +1,38 @@
-include "ex_type_pkg.sv";
-import ex_type_pkg::*;
+module alu_m #(parameter W = 1)
+ (
+  output logic [W-1:0] out, 
+  output logic zero,
+  input  logic [W-1:0] accum, 
+  input  logic [W-1:0] data, 
+  input  ex_type_pkg::opcode_e opcode, 
+  input  logic clk      
+ ) ;
 
-module alu_m();
+  timeunit 1ns ;
+  timeprecision 100ps ;
 
-	typedef enum logic [2:0] 
-	{ HLT, SKZ, ADD, 
-	AND, XOR, LDA, STO, JMP } opcode_e ;
+  import ex_type_pkg::* ;
 
-	input logic accum[7:0];
-	input logic data[7:0];
-	input logic opcode;
-	input logic clk;
-	output logic out;
-	logic zero;
+  always_ff @(negedge clk)
+    unique case ( opcode )
+      ADD: 
+	out <= accum + data;
+      AND:
+	out <= accum & data;
+      XOR:
+	out <= accum ^ data;
+      LDA:
+	out <= data;
+      HLT,
+      SKZ,
+      JMP,
+      STO:
+	out <= accum ;
+      default
+	out <= 8'bx ;
+    endcase
 
-	always_comb
-		if(accum == 3'b000)
-			begin
-				assign zero = 3'b001;
-			end
-		else
-			assign zero = 3'b000;
+  always_comb 
+    zero = ~(|accum) ;
 
-
-	always_ff @(negedge clk)
-	begin
-		unique case(opcode)
-			3'b000:
-				out = accum ; 
-			3'b001:
-				out = accum ;
-			3'b010:
-				out = data + accum ;
-			3'b011:
-				out = data & accum;
-			3'b100:
-				out = data ^ accum;
-			3'b101:
-				out = data;
-			4'b110:
-				out = accum ;
-			4'b111:
-				out = accum ;
-			default:
-				out = accum ; 
-		endcase
-	end
-
-endmodule: alu_m
+endmodule : alu_m
